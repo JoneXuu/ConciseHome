7f1724c6fb592646c356b240639a9368b2a3dbed
许蒋 <xujiang@anxhit.com>
2021-08-06 16:13:58 +0800

    feat：第一次上传浏览器主页项目

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ConciseHome
 A concise browser custom home page : 一个简洁的浏览器自定义主页
+
+各种各样的网站，平时找起来有点麻烦，
+因此自定义了一个搜索主页，自己随便放背景
+放引擎
---
+++ static/js/global.js
@@ -0,0 +1,61 @@
+var baseEngine = 'https://kaifa.baidu.com/searchPage?wd=',
+engineObj = {
+  'default':'https://kaifa.baidu.com/searchPage?wd=',
+  'baidu':'https://www.baidu.com/s?ie=UTF-8&wd=',
+  'csdn':'https://so.csdn.net/so/search?q=',
+  'zhihu':'https://www.zhihu.com/search?type=content&q=',
+  'sifou':'https://segmentfault.com/search?q=',
+},
+keyValue = '';
+
+// init 
+(function () {
+  setTimeout(() => {    
+    changeType();
+    const inputDom = document.querySelector("#mianSearchInput");
+    // setTimeout(() => {
+      // document.body.click();
+      // alert('获取焦点，只能这样获取浏览器的焦点了，害')
+    inputDom.focus();
+    // }, 10)
+    inputDom.onkeydown = function(e){
+      if (e.keyCode == 13) {
+        toSearch();
+      }
+  }
+  }, 0);
+})();
+
+function loadEngineIcon() {
+  
+}
+
+function inputListener(event) { // 输入监听
+  keyValue = event.value;
+}
+function toSearch() { // 确认搜索
+  if(keyValue){
+    // location.href = baseEngine + keyValue;
+    window.open(baseEngine + keyValue);
+  } else {
+    console.error('搜索内容不能为空');
+  }
+}
+
+function changeType(selDom) {
+  let type = localStorage.getItem('baseType') || 'default';
+  if (selDom) {
+    type = selDom.getAttribute('name');
+    localStorage.setItem('baseType', type);
+  }
+  baseEngine = engineObj[type];
+  const engineList = document.querySelector("#engineArea");// childNodes
+  for (let i = 0; i < engineList.childElementCount; i++) {
+    const oneDom = engineList.children[i];
+    oneDom.className = oneDom.classList.value.replace(/active/g, '').trim();
+    if (oneDom.getAttribute('name')===type) {
+      oneDom.className += ' active';
+    }
+  }
+
+}
